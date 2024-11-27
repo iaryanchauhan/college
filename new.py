0a1905b593207becf436b838aad9b7971f426b1b
iaryanchauhan <aryansinghc234@gmail.com>
2024-11-27 15:20:11 +0530

    jdi

---
+++ new.py
@@ -0,0 +1,9 @@
+# write a program in python to swap 2 numbers using 3rd variable
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+print(f"before swapping num1={a} and num2={b}")
+
+c=a
+a=b
+b=c
+print(f"after swapping num1={a} and num2={b}")
