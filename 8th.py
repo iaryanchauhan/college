fc4b1f19088fd0953d35bc8d1ce81937c392215f
iaryanchauhan <aryansinghc234@gmail.com>
2024-12-09 15:22:42 +0530

    new project

---
+++ 8th.py
@@ -0,0 +1,5 @@
+#find area of triangle
+a = float(input("enter the lenght of trangle:"))
+b = float(input("enter the height of trangle:"))
+c = 0.5*a*b
+print(f"area of tarangle is {c}")
---
+++ 9th.py
@@ -0,0 +1,4 @@
+#wap to print a random number
+
+import random
+print("random number is: ",random.randint(1,1000))
