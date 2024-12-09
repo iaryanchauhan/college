03c69a1839ed4b2e54757a796ed16b7f6b643eb2
iaryanchauhan <aryansinghc234@gmail.com>
2024-12-09 15:25:41 +0530

    s

--- 1st.py
+++ 1st.py
@@ -1,4 +1,4 @@
-# write a program in python to find the largest among three numbers enterd by the user.
+# write a program in python to swap 2 number using 3rd variable 
 a=int(input("enter number:"))
 b=int(input("enter number:"))
 print(f"before swapping num1={a} and num2={b}".format(a,b))
--- first.py
+++
@@ -1,9 +0,0 @@
-# write a program in python to swap 2 number using 3rd variable 
-a=int(input("enter number:"))
-b=int(input("enter number:"))
-print(f"before swapping num1={a} and num2={b}".format(a,b))
-
-c=a
-a=b
-b=c
-print(f"after swapping num1={a} and num2={b}".format(a,b))
